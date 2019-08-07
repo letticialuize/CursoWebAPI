6e4cb9bd22bf263cd2c0b26c71d26f4b039a4abf
Lettícia Luize <letticialuize@gmail.com>
2019-08-07 13:52:35 -0300

    Alteração na classe Aluno no DataAnnotation.

--- WebAPI/Models/AlunoDTO.cs
+++ WebAPI/Models/AlunoDTO.cs
@@ -11,12 +11,16 @@ namespace WebAPI.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "O nome é de preenchimento obrigatório")]
+        [StringLength(50, ErrorMessage = "Nome tem no mínimo 2 caracteres e no máximo 50", MinimumLength = 2)]
         public string Nome { get; set; }
         public string Sobrenome { get; set; }
         public string Telefone { get; set; }
+
+        [RegularExpression(@"[0-9]{4}\-[0-9]{2}", ErrorMessage = "A data está fora do formato YYYYY-MM" )]
         public string Data { get; set; }
 
         [Required(ErrorMessage = "O RA é de preenchimento obrigatório")]
+        [Range(1,9099, ErrorMessage = "O intervalo para cadastro de RA está entre 1 e 9099")]
         public int? RegistroAcademico { get; set; }
     }
 }
